01457ac668e53abd3f7af67dc35542ac6385797f
Tobias Ottenweller <mail@ottenweller.net>
2013-07-11 23:33:47 +0200

    Commented out unused methods.

--- src/me/drayshak/WorldInventories/Group.java
+++ src/me/drayshak/WorldInventories/Group.java
@@ -9,20 +9,21 @@ public class Group
     private String name;
     private List<String> worlds;
     private GameMode gamemode;
-    
-    public Group(String tName)
-    {
-        this.name = tName;
-        this.worlds = new ArrayList<String>();
-        this.gamemode = GameMode.SURVIVAL;
-    }
-    
-    public Group(String tName, List<String> tWorlds)
-    {
-        this.name = tName;
-        this.worlds = tWorlds;
-        this.gamemode = GameMode.SURVIVAL;
-    }
+
+    // TODO: remove unsused methods?
+//    public Group(String tName)
+//    {
+//        this.name = tName;
+//        this.worlds = new ArrayList<String>();
+//        this.gamemode = GameMode.SURVIVAL;
+//    }
+//
+//    public Group(String tName, List<String> tWorlds)
+//    {
+//        this.name = tName;
+//        this.worlds = tWorlds;
+//        this.gamemode = GameMode.SURVIVAL;
+//    }
     
     public Group(String tName, List<String> tWorlds, GameMode mode)
     {
@@ -31,51 +32,51 @@ public class Group
         this.gamemode = mode;        
     }
     
-    public void addWorld(String world)
-    {
-        if(!this.worlds.contains(world))
-        {
-            this.worlds.add(world);
-        }
-    }
+//    public void addWorld(String world)
+//    {
+//        if(!this.worlds.contains(world))
+//        {
+//            this.worlds.add(world);
+//        }
+//    }
     
     public String getName()
     {
         return this.name;
     }
     
-    public void setName(String tName)
-    {
-        this.name = tName;
-    }
+//    public void setName(String tName)
+//    {
+//        this.name = tName;
+//    }
     
     public List<String> getWorlds()
     {
         return this.worlds;
     }
     
-    public void setWorlds(List<String> tWorlds)
-    {
-        this.worlds = tWorlds;
-    }
+//    public void setWorlds(List<String> tWorlds)
+//    {
+//        this.worlds = tWorlds;
+//    }
     
     public GameMode getGameMode()
     {
         return this.gamemode;
     }
     
-    public void setGameMode(GameMode mode)
-    {
-        this.gamemode = mode;
-    }
-    
-    public boolean contains(String tWorldName)
-    {
-        return this.worlds.contains(tWorldName);
-    }
-    
-    public static String getName(Group g)
-    {
-        return g.getName();
-    }
+//    public void setGameMode(GameMode mode)
+//    {
+//        this.gamemode = mode;
+//    }
+//
+//    public boolean contains(String tWorldName)
+//    {
+//        return this.worlds.contains(tWorldName);
+//    }
+//
+//    public static String getName(Group g)
+//    {
+//        return g.getName();
+//    }
 }
