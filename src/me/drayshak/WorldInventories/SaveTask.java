ab335943d76cefa2becbf9c22be0e2ef4e2e80c5
Tobias Ottenweller <mail@ottenweller.net>
2013-07-17 19:43:13 +0200

    Fixed indention. Refactoring for shorter lines.

--- src/me/drayshak/WorldInventories/SaveTask.java
+++ src/me/drayshak/WorldInventories/SaveTask.java
@@ -8,16 +8,16 @@ public class SaveTask extends TimerTask
     
     public SaveTask(WorldInventories plugin)
     {
-	this.plugin = plugin;
+	    this.plugin = plugin;
     }
     
     @Override
     public void run()
     {
-        if(plugin.getConfig().getBoolean("outputtimertoconsole"))
-	{
-	    WorldInventories.logStandard("Timer: Saving player information. New save due in " + plugin.getConfig().getInt("saveinterval") + " seconds.");
-	}
+        if (plugin.getConfig().getBoolean("outputtimertoconsole")) {
+            int saveTimeInterval = plugin.getConfig().getInt("saveinterval");
+	        WorldInventories.logStandard("Timer: Saving player information. New save due in " + saveTimeInterval + " seconds.");
+	    }
         
         plugin.savePlayers(plugin.getConfig().getBoolean("outputtimertoconsole"));
     }
