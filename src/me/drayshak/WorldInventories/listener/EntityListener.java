df7f5c2538e36679985ec5ceb42725b220d1eced
Tobias Ottenweller <mail@ottenweller.net>
2013-07-17 19:49:15 +0200

    Cleanup for the EnityListener class.

--- src/me/drayshak/WorldInventories/listener/EntityListener.java
+++ src/me/drayshak/WorldInventories/listener/EntityListener.java
@@ -1,13 +1,10 @@
 package me.drayshak.WorldInventories.listener;
 
 import java.util.HashMap;
-import me.drayshak.WorldInventories.Group;
-import me.drayshak.WorldInventories.InventoryStoredType;
-import me.drayshak.WorldInventories.InventoryLoadType;
-import me.drayshak.WorldInventories.PlayerStats;
-import me.drayshak.WorldInventories.WorldInventories;
+
+import me.drayshak.WorldInventories.*;
 import me.drayshak.WorldInventories.api.WorldInventoriesAPI;
-import org.bukkit.entity.Entity;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -26,32 +23,34 @@ public class EntityListener implements Listener
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event)
     {
-        Entity entity = event.getEntity();
+        if (!(event.getEntity() instanceof Player)) {
+            return;
+        }
 
-        if (entity instanceof Player) {
-            Player player = (Player)entity;
-            String worldName = player.getWorld().getName();
-            
-            if (WorldInventories.exempts.contains(player.getName().toLowerCase()))
-            {
-                WorldInventories.logDebug("Ignoring exempt player death: " + player.getName());
-                return;
-            }
-            
-            Group togroup = WorldInventoriesAPI.findGroup(worldName);
+        Player player = (Player)event.getEntity();
 
-            WorldInventories.logDebug("Player " + player.getName() + " died in world " + worldName + ", emptying inventory for group: " + togroup.getName());
+        String worldName = player.getWorld().getName();
+        String playerName = player.getName();
 
-            // Make the saved inventory blank so players can't duplicate by switching worlds and picking items back up
-            HashMap<Integer, ItemStack[]> tosave = new HashMap<Integer, ItemStack[]>();
-            tosave.put(InventoryStoredType.ARMOUR, new ItemStack[4]);
-            tosave.put(InventoryStoredType.INVENTORY, new ItemStack[36]);
-            
-            plugin.savePlayerInventory(player.getName(), togroup, InventoryLoadType.INVENTORY, tosave);
-            
-            if(plugin.getConfig().getBoolean("dostats")) {
-                plugin.savePlayerStats(player.getName(), togroup, new PlayerStats(20, 20, 0, 0, 0, 0F, null));
-            }
+
+        if (WorldInventories.exempts.contains(playerName.toLowerCase())) {
+            WorldInventories.logDebug("Ignoring exempt player death: " + playerName);
+            return;
+        }
+
+        Group toGroup = WorldInventoriesAPI.findGroup(worldName);
+
+        WorldInventories.logDebug("Player " + playerName + " died in world " + worldName + ", emptying inventory for group: " + toGroup.getName());
+
+        // Make the saved inventory blank so players can't duplicate by switching worlds and picking items back up
+        HashMap<Integer, ItemStack[]> toSave = new HashMap<Integer, ItemStack[]>();
+        toSave.put(InventoryStoredType.ARMOUR, new ItemStack[4]);
+        toSave.put(InventoryStoredType.INVENTORY, new ItemStack[36]);
+
+        plugin.savePlayerInventory(playerName, toGroup, InventoryLoadType.INVENTORY, toSave);
+
+        if (plugin.getConfig().getBoolean("dostats")) {
+            plugin.savePlayerStats(playerName, toGroup, new PlayerStats(20, 20, 0, 0, 0, 0F, null));
         }
     }
 }
