375782c888e15b3f3df7f23e82962700527f60b7
Tobias Ottenweller <mail@ottenweller.net>
2013-07-18 10:39:58 +0200

    Fixed typos. Converted attributes to camel case.

--- src/me/drayshak/WorldInventories/PlayerStats.java
+++ src/me/drayshak/WorldInventories/PlayerStats.java
@@ -6,93 +6,93 @@ import org.bukkit.potion.PotionEffect;
 public class PlayerStats
 {
     private double health;
-    private int foodlevel;
-    private float exhausation;
+    private int foodLevel;
+    private float exhaustion;
     private float saturation;
     
     private int level;
     private float exp;
     
-    private Collection<PotionEffect> potioneffects;
+    private Collection<PotionEffect> potionEffects;
     
-    public PlayerStats(double health, int foodlevel, float exhaustion, float saturation, int level, float exp, Collection<PotionEffect> potioneffects)
+    public PlayerStats(double health, int foodLevel, float exhaustion, float saturation, int level, float exp, Collection<PotionEffect> potionEffects)
     {
         this.health = health;
-        this.foodlevel = foodlevel;
-        this.exhausation = exhaustion;
+        this.foodLevel = foodLevel;
+        this.exhaustion = exhaustion;
         this.saturation = saturation;
         this.level = level;
         this.exp = exp;
-        this.potioneffects = potioneffects;
+        this.potionEffects = potionEffects;
     }
     
     public Collection<PotionEffect> getPotionEffects()
     {
-        return this.potioneffects;
+        return this.potionEffects;
     }
     
-    public void setPotionEffects(Collection<PotionEffect> potioneffects)
-    {
-        this.potioneffects = potioneffects;
-    }
+//    public void setPotionEffects(Collection<PotionEffect> potionEffects)
+//    {
+//        this.potionEffects = potionEffects;
+//    }
     
     public double getHealth()
     {
         return this.health;
     }
     
-    public void setHealth(double health)
-    {
-        this.health = health;
-    }
-    
+//    public void setHealth(double health)
+//    {
+//        this.health = health;
+//    }
+//
     public int getFoodLevel()
     {
-        return this.foodlevel;
+        return this.foodLevel;
     }
     
-    public void setFoodLevel(int foodlevel)
-    {
-        this.foodlevel = foodlevel;
-    }   
+//    public void setFoodLevel(int foodlevel)
+//    {
+//        this.foodLevel = foodlevel;
+//    }
     
     public float getExhaustion()
     {
-        return this.exhausation;
+        return this.exhaustion;
     }
     
-    public void setExhaustion(float exhaustion)
-    {
-        this.exhausation = exhaustion;
-    }
+//    public void setExhaustion(float exhaustion)
+//    {
+//        this.exhaustion = exhaustion;
+//    }
     
     public float getSaturation()
     {
         return this.saturation;
     }
     
-    public void setSaturation(float saturation)
-    {
-        this.saturation = saturation;
-    }
+//    public void setSaturation(float saturation)
+//    {
+//        this.saturation = saturation;
+//    }
     
     public int getLevel()
     {
         return this.level;
     }
     
-    public void setLevel(int level)
-    {
-        this.level = level;
-    }
+//    public void setLevel(int level)
+//    {
+//        this.level = level;
+//    }
     
     public float getExp()
     {
         return this.exp;
     }
     
-    public void setExp(float exp)
-    {
-        this.exp = exp;
-    }
+//    public void setExp(float exp)
+//    {
+//        this.exp = exp;
+//    }
 }
