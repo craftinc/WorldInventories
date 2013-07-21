35c3431c3ffc34bd7cea9a8710dc6ade4cf70594
Tobias Ottenweller <mail@ottenweller.net>
2013-07-21 14:24:09 +0200

    Simplified the SaveTask class.

--- src/de/craftinc/inventories/persistence/SaveTask.java
+++ src/de/craftinc/inventories/persistence/SaveTask.java
@@ -12,12 +12,13 @@ public class SaveTask extends TimerTask
     public void run()
     {
         WorldInventories plugin = WorldInventories.getSharedInstance();
+        boolean logSaveTimerMessages = plugin.getConfig().getBoolean(ConfigurationKeys.logSaveTimerMessagesKey);
 
-        if (plugin.getConfig().getBoolean(ConfigurationKeys.logSaveTimerMessagesKey)) {
+        if (logSaveTimerMessages) {
             int saveTimeInterval = plugin.getConfig().getInt(ConfigurationKeys.saveTimerIntervalKey);
             Logger.logStandard("Timer: Saving player information. New save due in " + saveTimeInterval + " seconds.");
 	    }
 
-        InventoryPersistenceManager.savePlayers(plugin.getConfig().getBoolean(ConfigurationKeys.logSaveTimerMessagesKey));
+        InventoryPersistenceManager.savePlayers(logSaveTimerMessages);
     }
 }
