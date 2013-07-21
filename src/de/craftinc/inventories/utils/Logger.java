b7f09ef8b4b1240c4b582f7d1315634bf8d35b08
Tobias Ottenweller <mail@ottenweller.net>
2013-07-21 19:27:28 +0200

    Print plugin name and version when logging to console.

--- src/de/craftinc/inventories/utils/Logger.java
+++ src/de/craftinc/inventories/utils/Logger.java
@@ -9,21 +9,33 @@ import java.util.logging.Level;
 public class Logger
 {
     protected static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger("Minecraft");
+    protected static String pluginString = null;
+
+
+    protected static String getPluginString()
+    {
+        if (pluginString == null) {
+            Plugin plugin = Plugin.getSharedInstance();
+            pluginString = "[" + plugin.getDescription().getFullName() + "] {0}";
+        }
+
+        return pluginString;
+    }
 
 
     public static void logStandard(String line)
     {
-        logger.log(Level.INFO, "[Plugin] {0}", line);
+        logger.log(Level.INFO, getPluginString(), line);
     }
 
     public static void logError(String line)
     {
-        logger.log(Level.SEVERE, "[Plugin] {0}", line);
+        logger.log(Level.SEVERE, getPluginString(), line);
     }
 
     public static void logDebug(String line)
     {
-        logger.log(Level.FINEST, "[Plugin] {0}", line);
+        logger.log(Level.FINEST, getPluginString(), line);
     }
 
     /**
